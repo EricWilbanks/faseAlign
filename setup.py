2de2c0fc3d4a822ffc5a7aeb310306eb637bb3db
BCE <bce@berkeley.edu>
2017-06-28 18:09:40 -0700

    setup.py Updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='faseAlign',
-    version='1.0.0.dev4',
+    version='1.0.0.dev5',
     description='Command Line python module for Force Aligning Spanish using HTK',
     url='https://github.com/EricWilbanks/faseAlign',
     author='Eric Wilbanks',
