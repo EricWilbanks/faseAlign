68cfceaf85fef00658e5207caa3a905e73b91652
EricWilbanks <wilbanks.ericw@gmail.com>
2017-06-29 15:19:11 -0700

    script execution fix

--- faseAlign/faseAlign.py
+++ faseAlign/faseAlign.py
@@ -4,6 +4,7 @@
 # Unix allows for execution of files without .py, but Windows does not. 
 # This workaround allows faseAlign to be called (without extension) from the command line in an OS-independent fashion 
 import faseAlign
+import argparse
 
 if __name__ == '__main__':
 
