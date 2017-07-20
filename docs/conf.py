4142cbf4da5fc495309df9fb3db5a5c619505307
BCE <bce@berkeley.edu>
2017-07-19 17:09:24 -0700

    versioning updates

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = 'Eric Wilbanks'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.1'
+version = '0.1'
 # The full version, including alpha/beta/rc tags.
 release = '0.1.1'
 
